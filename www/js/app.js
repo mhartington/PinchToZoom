1949ef4b9b094bb750584d3fa6a6fdc63efaec83
Mike Hartington <mikehartington@gmail.com>
2014-10-30 09:46:26 -0400

    more photo-gallery-like

--- www/js/app.js
+++ www/js/app.js
@@ -9,46 +9,56 @@ angular.module('starter', ['ionic'])
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
-    if(window.StatusBar) {
+    if (window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
 
- .controller('MainCtrl', function($scope, $ionicModal) {
+.controller('MainCtrl', function($scope, $ionicModal) {
   $scope.hide = [{
-    bars: true 
+    bars: true
   }]
-      $scope.data = {};
-      $scope.grids = [{
-        src: 'http://lorempixel.com/400/200/sports/1/',
-      }, {
-        src: 'http://lorempixel.com/400/200/sports/2/',
-      }, {
-        src: 'http://lorempixel.com/400/200/sports/3/',
-      }, {
-        src: 'http://lorempixel.com/400/200/sports/4/',
-      }, {
-        src: 'http://lorempixel.com/400/200/sports/5/',
-      }, {
-        src: 'http://lorempixel.com/400/200/sports/6/',
-      }];
+  $scope.data = {};
+  $scope.grids = [{
+    id: 0,
+    src: 'http://lorempixel.com/400/200/sports/1/',
+  }, {
+    id: 1,
+    src: 'http://lorempixel.com/400/200/sports/2/',
+  }, {
+    id: 2,
+    src: 'http://lorempixel.com/400/200/sports/3/',
+  }, {
+    id: 3,
+    src: 'http://lorempixel.com/400/200/sports/4/',
+  }, {
+    id: 4,
+    src: 'http://lorempixel.com/400/200/sports/5/',
+  }, {
+    id: 5,
+    src: 'http://lorempixel.com/400/200/sports/6/',
+  }];
 
-      $ionicModal.fromTemplateUrl('modal.html', function(modal) {
-        $scope.gridModal = modal;
-      }, {
-        scope: $scope,
-        animation: 'slide-in-up'
-      });
-      $scope.openModal = function(selected) {
-        $scope.data.selected = selected;
-        $scope.gridModal.show();
-      };
-      $scope.closeModal = function() {
-        $scope.gridModal.hide();
-        $scope.hide.bars=false;
-      };
-     });
+  $ionicModal.fromTemplateUrl('templates/modal.html', function(modal) {
+    $scope.gridModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  $scope.openModal = function(selected) {
+    console.log(selected.id);
+    $scope.data.selected = selected.id;
+
+    $scope.gridModal.show();
+  };
+
+  $scope.closeModal = function() {
+    $scope.gridModal.hide();
+    $scope.hide.bars = false;
+  };
+});
